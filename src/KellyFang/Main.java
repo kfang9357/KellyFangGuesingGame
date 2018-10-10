89acca433fcc2f790f516954cac9310afd98166b
BT_1N3_27 <kfang9357@bths.edu>
2018-10-10 08:41:52 -0400

    make separate class files for different objectives

--- src/KellyFang/Main.java
+++ src/KellyFang/Main.java
@@ -3,9 +3,10 @@ package KellyFang;
 import java.util.Scanner;
 public class Main {
 
+// objective 1
     public static void main(String[] args) {
 	double num = Math.random();
-	int random  = (int)(num*101);
+	int random  = (int)(num*10);
 
 	Scanner input = new Scanner(System.in);
 	int guess;
@@ -18,8 +19,27 @@ public class Main {
         guess = input.nextInt();
 
     }
-    System.out.print("You got it! The word is "+guess);
+    System.out.print("You got it! The number is "+guess);
+
+	//objective2
+	Scanner input2 = new Scanner(System.in);
+	int guess2 = 50;
+	System.out.print("is your nummber "+50);
+	String correct = input2.nextLine();
+	while (!input2.equals("correct"));
+		{
+			if (correct.equals("lower"))
+			{
+				guess2 = (int) guess2/2;
+			}
+			else
+			{
+				guess2 = (int) guess2*(3/2);
+			}
+			System.out.print("is your number "+ guess2);
+		}
     }
+
 }
 
 
